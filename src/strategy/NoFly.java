f9204730279e422bb1f13ddf7656d8aec82242cc
Primary-Lin <ljs821462526@163.com>
2016-03-30 14:41:33 +0800

    修改NoFly类

--- src/strategy/NoFly.java
+++ src/strategy/NoFly.java
@@ -4,7 +4,7 @@ public class NoFly implements FlyBehavior {
 
 	public void fly() {
 		// TODO Auto-generated method stub
-		System.out.println("I can not fly");
+		System.out.println("I can not fly without wings");
 	}
 
 }
